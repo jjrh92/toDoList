630664b4cafc2f69c785490a0da375af90d3964e
Julio Reyes <juliojesusreyes@gmail.com>
2023-03-11 19:31:01 -0300

    Actualizando color del placeholder

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 // Inicio
 
+const tarjeta = document.getElementById ("tarjeta");
+const boton_agregar_tarea = document.getElementById ("boton_agregar_tarea");
+const texto_agregar_tarea = document.getElementById ("texto_agregar_tarea");
+
+function CrearTarea () {
+
+    console.log (texto_agregar_tarea.value);
+    alert (texto_agregar_tarea.value);
+    texto_agregar_tarea.value = "";
+    
+
+};
+
+boton_agregar_tarea.addEventListener ("click", CrearTarea);
+
 // Fin
