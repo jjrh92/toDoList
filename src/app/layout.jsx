dcee50e1ef2dd968902a85f058139277e45d359f
Julio Reyes <juliojesusreyes@gmail.com>
2024-12-17 22:30:50 +0100

    Layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,7 +6,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Task Manager 2.0",
+  title: "TaskMate",
   description: "Check out www.julioreyes.dev",
 };
 
